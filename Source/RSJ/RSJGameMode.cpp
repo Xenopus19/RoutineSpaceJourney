8d2a1b1c453b56cffb9fe870c11145b4b5e0574e
Xenopus19 <82256288+Xenopus19@users.noreply.github.com>
2024-11-16 12:41:57 +0200

    Project created

---
+++ Source/RSJ/RSJGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RSJGameMode.h"
+#include "RSJCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ARSJGameMode::ARSJGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/RSJ/RSJGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RSJGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARSJGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARSJGameMode();
+};
+
+
+
